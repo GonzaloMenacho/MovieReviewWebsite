cbedf33f2b1b08b3348e5f33216533a0176b9e19
Michael DAgostino <dagostinomichael00@gmail.com>
2023-02-22 14:14:35 -0500

    Added baseline Review calls to database. Takes the input query and calls GetOnBody. Right now, it seems to be calling reviews unrelated to the search term.

--- src/components/searchreviewusingmovie.js
+++ src/components/searchreviewusingmovie.js
@@ -15,7 +15,8 @@ class SearchBar extends React.Component {
             title: "",
             rating: "",
             description: "",
-            posts: ""
+            movieposts: "",
+            reviewposts: ""
         };
     }
 
@@ -32,14 +33,27 @@ class SearchBar extends React.Component {
                         Get Results
                     </button>
                 </div>
-                {this.state.posts &&
-                    <div className="all-reviews-display">
-                        {this.state.posts.map((post) => {
+                {this.state.movieposts &&
+                    <div className="all-movies-display">
+                        {this.state.movieposts.map((post) => {
                             return (
                                 <div className="post-card" key={post.movieID}>
                                     <h2 className="post-title">{post.title}</h2>
                                     <p className="post-rating">Rating: {post.movieIMDbRating}</p>
                                     <p className="post-desc">Description: {post.description}</p>
+                                </div>
+                            );
+                        })}
+                    </div>
+                }
+
+                {this.state.reviewposts &&
+                    <div className="all-reviews-display">
+                        {this.state.reviewposts.map((post) => {
+                            return (
+                                <div className="post-card" key={post.movieID}>
+                                    <h2 className="post-title">{post.reviewTitle}</h2>
+                                    <p className="post-rating">Rating: {post.reviewBody}</p>
                                 </div>
                             );
                         })}
@@ -59,7 +73,13 @@ class SearchBar extends React.Component {
     getReview = () => {
         client.get(`Movies/title?m_title=${this.state.searchterm}`).then((response) => {
             this.setState({
-                posts: response.data,
+                movieposts: response.data,
+            })
+        });
+        
+        client.get(`Reviews/GetByBody?m_text=${this.state.searchterm}&searchOnTitle=true`).then((response) => {
+            this.setState({
+                reviewposts: response.data,
             })
         });
     }
