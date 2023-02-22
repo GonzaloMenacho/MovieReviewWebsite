b875d79e4c4bfb39c3f717c3af3611bc38bee843
Michael DAgostino <dagostinomichael00@gmail.com>
2023-02-22 13:44:00 -0500

    Search bar now searches on movies.

--- src/components/searchreviewusingmovie.js
+++ src/components/searchreviewusingmovie.js
@@ -6,57 +6,64 @@ const client = axios.create({
     header: { 'X-Custom-Header': 'foobar' }
 });
 
-class Reviews extends React.Component {
+class SearchBar extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            movie: "",
+            searchterm: "",
             title: "",
             rating: "",
-            description: ""
+            description: "",
+            posts: ""
         };
     }
 
     render() {
         return (
+            <div className="search">
 
-            <div className="reviews">
-
-                <div id="movie">
+                <div id="search-bar">
                     <input onChange={this.changeValue}
-                        type="text" placeholder="Search a Movie or Review..." value={this.state.movie} />
+                        type="text" placeholder="Search a Movie or Review..." value={this.state.searchterm} />
                 </div>
-                <div className="button">
+                <div className="search-button">
                     <button onClick={this.getReview}>
                         Get Results
                     </button>
                 </div>
-{/*                <div>
-                    <h1>Movie: {this.state.title}</h1>
-                    <h3>Rating: {this.state.rating}</h3>
-                    <h3>Description: {this.state.description}</h3>
-        </div>*/}
+                {this.state.posts &&
+                    <div className="all-reviews-display">
+                        {this.state.posts.map((post) => {
+                            return (
+                                <div className="post-card" key={post.movieID}>
+                                    <h2 className="post-title">{post.title}</h2>
+                                    <p className="post-rating">Rating: {post.movieIMDbRating}</p>
+                                    <p className="post-desc">Description: {post.description}</p>
+                                </div>
+                            );
+                        })}
+                    </div>
+                }
             </div>              
         );
     }
 
     changeValue = (event) => {
         this.setState({
-            movie: event.target.value
+            searchterm: event.target.value
         });
     }
 
+    //'https://localhost:7035/api/Movies/title?m_title=avengers'
     getReview = () => {
-        client.get(`/Movies/${this.state.movie}`).then((response) => {
+        client.get(`Movies/title?m_title=${this.state.searchterm}`).then((response) => {
             this.setState({
-                title: response.data.title,
-                rating: response.data.rating,
-                description: response.data.description
+                posts: response.data,
             })
         });
     }
 
 }
 
-export default Reviews;
+export default SearchBar;
