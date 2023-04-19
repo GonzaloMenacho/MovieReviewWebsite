42071e21692168dd8a019b79f30c39113d22f3c6
Michael DAgostino <dagostinomichael00@gmail.com>
2023-04-19 00:29:15 -0400

    Changed nomovies.js message to be slightly clearer

--- src/components/nomovies.js
+++ src/components/nomovies.js
@@ -4,18 +4,36 @@ import noMoviesImage from '../images/death.png'
 
 export default function NoMovies() {
     return (
-        <Grid container justifyContent="center" alignItems="center" height="100%">
-          <Box sx={{ p: 6 }}>
-            <Typography variant="h3">
-              Hmmm... there were no movie results for "your search".
-            </Typography>
-            <Typography variant="body1">
-              Check your search for typos or try searching for a different movie title.
-            </Typography>
-            <Box style={{ display: 'flex', justifyContent: 'center' }}>
-            <img src={noMoviesImage} alt="No movies found" style={{  marginTop: 16, maxWidth: '100%', width: 500  }} />
+        <Grid container
+            justifyContent="center"
+            alignItems="center"
+            height="100%">
+            <Box sx={{
+                p: 6
+            }}>
+                <Typography
+                    variant="h4">
+                We couldn't find any results for your search.
+                </Typography>
+                <hr />
+                <Typography
+                    variant="body1"
+                    component="span"
+                >
+                    Your search was a bit&nbsp;
+                    <span style={{color: "red"}}>
+                        sus...
+                    </span>
+                    Check for typos or try searching for something else.
+                </Typography>
+                <Box
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center'
+                    }}>
+                <img src={noMoviesImage} alt="No movies found" style={{  marginTop: 16, maxWidth: '100%', width: 500  }} />
+                </Box>
             </Box>
-          </Box>
-            </Grid>
+        </Grid>
   );
 }
