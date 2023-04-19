27d622239fc85f0f53b23bd7df33c425cb26eab3
Michael DAgostino <dagostinomichael00@gmail.com>
2023-04-19 00:04:14 -0400

    MovieReviewCard now looks sexy af and has all the review details. React screen sizing also mostly fixed outside of extreme edge cases.

--- src/components/moviereviewcard.js
+++ src/components/moviereviewcard.js
@@ -8,6 +8,9 @@ import Box from '@mui/material/Box';
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { ThumbUp, ThumbDown, Feedback } from '@mui/icons-material';
+import { WidthFull } from '../../node_modules/@mui/icons-material/index';
 
 class MovieReviewCard extends Component {
   constructor(props) {
@@ -25,9 +28,9 @@ class MovieReviewCard extends Component {
     const { key, movie, reviews } = this.props;
 
     return (
-      <Box key={key} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '1rem' }}>
+      <Box key={key} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '1rem'}}>
         <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'stretch', width: '80%' }}>
-          <Card sx={{ width: '15%', bgcolor: 'grey.100' }}>
+          <Card sx={{ width: '15%', bgcolor: 'grey.100', flexShrink: 0, minWidth: 150 }}>
             <CardActionArea>
               <CardContent>
                     <CardMedia
@@ -39,15 +42,113 @@ class MovieReviewCard extends Component {
               </CardContent>
             </CardActionArea>
           </Card>
-
-          <Box sx={{ flex: '1', display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+                <Box sx={{
+                    flex: '1',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'flex-start'
+                }}>
             {reviews.map((review, index) => (
-              <Accordion key={index} expanded={this.state.expanded === `panel${index}`} onChange={this.handleChange(`panel${index}`)}>
-                <AccordionSummary aria-controls={`panel${index}a-content`} id={`panel${index}a-header`}>
-                  <Typography>{review.reviewTitle}</Typography>
+                <Accordion
+                    key={index}
+                    sx={{
+                        minWidth: WidthFull * .20,
+                        maxWidth: WidthFull * .80
+                    }}
+                    expanded={this.state.expanded === `panel${index}`}
+                    onChange={this.handleChange(`panel${index}`)}>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon /> }
+                        aria-controls={`panel${index}a-content`}
+                        id={`panel${index}a-header`}>
+                        <Typography sx={{
+                            color: review.userRating > 7 ? "royalblue" : review.userRating < 4 ? "red" : "teal",
+                            width: '8%',
+                            minWidth: 60,
+                            whiteSpace: 'nowrap',
+                            flexShrink: 0
+                        }}>
+                            {review.userRating} / 10
+                        </Typography>
+                        <>
+                            {review.userRating > 7 ? (
+                                <ThumbUp sx={{
+                                    color: 'blue',
+                                    width: '5%',
+                                    minWidth: 40,
+                                    flexShrink: 0
+                                }} />
+                            ) : review.userRating < 4 ? (
+                                <ThumbDown sx={{
+                                    color: 'red',
+                                    width: '5%',
+                                    minWidth: 40,
+                                    flexShrink: 0
+                                }} />
+                                ) : <Feedback sx={{
+                                    color: 'teal',
+                                    width: '5%',
+                                    minWidth: 40,
+                                    flexShrink: 0
+                                }} />}
+                        </>
+                        <Typography
+                            sx={{ color: 'black' }}>
+                            {review.reviewTitle}
+                        </Typography>
                 </AccordionSummary>
-                <AccordionDetails>
-                  <Typography>{review.reviewBody}</Typography>
+                    <AccordionDetails>
+                        <Typography
+                            sx={{ color: 'black'}}
+                            variant="h6"
+                            component="span"
+                            gutterBottom
+                        >
+                            <span style={{
+                                fontWeight: 'bold',
+                                color: review.userRating > 7 ? "royalblue" : review.userRating < 4 ? "red" : "teal",
+                            }}>{review.username}</span> says...
+                        </Typography>
+                        <Box sx={{padding: '16px'}}>
+                            <Card
+                                sx={{
+                                
+                                }}
+                                variant="outlined"
+                                gutterBottom
+                            >
+                            <CardContent>
+                                <Typography sx={{
+                                    color: 'text.primary'
+                                }}>
+                                    {review.reviewBody}
+                                </Typography>
+                            </CardContent>
+                            </Card>
+                        </Box>
+                        <Typography
+                            sx={{ color: 'text.secondary' }}
+                            variant="h6"
+                            gutterBottom
+                        >
+                            {new Date(review.dateofReview).toLocaleString('en-US', {
+                                month: 'long',
+                                day: 'numeric',
+                                year: 'numeric'
+                            })}
+                        </Typography>
+                        <Typography
+                            sx={{
+                            color: 'text.secondary'
+                            }}
+                            component="span"
+                        >
+                            <span style={{
+                                fontWeight: 'bold',
+                                color: review.usefulnessVote / review.totalVotes > .85 ? "royalblue" : review.usefulnessVote / review.totalVotes < .55 ? "red" : "teal",
+                            }}>
+                            {review.usefulnessVote}</span> out of {review.totalVotes} users found this review helpful.
+                        </Typography>
                 </AccordionDetails>
               </Accordion>
             ))}
